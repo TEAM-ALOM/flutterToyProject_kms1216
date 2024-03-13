6ec6545fee5ed86cdadb3560b655fd44b2f11b80
김민석 <minseok5640@naver.com>
2024-03-13 14:05:16 +0900

    adsf

---
+++ lib/homescreen.dart
@@ -0,0 +1,168 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/todolist.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.check),
+                  SizedBox(width: 10,),
+                  Text("ToDoList",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 30),)
+                ],
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context)=> const todolist()));
+                    },
+                    icon: Icon(Icons.add),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                      child: SizedBox(
+                        height: 100,
+                        child: ExpansionTile(
+                          title: Text(
+                            "오늘의 할 일",
+                            style: TextStyle(
+                              fontSize: 20,
+                            ),
+                          ),
+                          tilePadding: EdgeInsets.zero,
+                          children: [
+                            ListTile(
+                              title: Text("할 일을 작성해주세요"),
+                            ),
+                          ],
+                        ),
+                      ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 100,
+                      child: ExpansionTile(
+                        title: Text(
+                          "일주일 간 해야할 일",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        tilePadding: EdgeInsets.zero,
+                        children: [
+                          ListTile(
+                            title: Text(""),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 100,
+                      child: ExpansionTile(
+                        title: Text(
+                          "이번 달의 할 일",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        tilePadding: EdgeInsets.zero,
+                        children: [
+                          ListTile(
+                            title: Text(""),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: SizedBox(
+                      height: 100,
+                      child: ExpansionTile(
+                        title: Text(
+                          "지금 할 일",
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
+                        ),
+                        tilePadding: EdgeInsets.zero,
+                        children: [
+                          ListTile(
+                            title: Text(""),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list/homescreen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
